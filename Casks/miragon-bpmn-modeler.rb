72a4e97b440b0f6ae84684020d347e8134b12480
Marco Schäck <marco.schaeck@outlook.de>
2026-05-12 13:31:14 +0200

    fix(miragon-bpmn-modeler): use actual GitHub asset URL (spaces → dots) for v0.0.6

--- Casks/miragon-bpmn-modeler.rb
+++ Casks/miragon-bpmn-modeler.rb
@@ -2,7 +2,7 @@ cask "miragon-bpmn-modeler" do
   version "0.0.6"
   sha256 "fcfbb8714f7f9a84172362cdd838202e2dfb051655f72cdb3715792e65c9ad7b"
 
-  url "https://github.com/Miragon/bpmn-modeler/releases/download/standalone-v0.0.6/Miragon%20BPMN%20Modeler-0.0.6-arm64.dmg"
+  url "https://github.com/Miragon/bpmn-modeler/releases/download/standalone-v0.0.6/Miragon.BPMN.Modeler-0.0.6-arm64.dmg"
   name "Miragon BPMN Modeler"
   desc "Standalone BPMN/DMN process modeler"
   homepage "https://github.com/Miragon/bpmn-modeler"
